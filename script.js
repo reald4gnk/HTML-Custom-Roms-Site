5ef6b07f8138aa149642e8f47f5131480557255d
reald4gnk <dragonek.gg@gmail.com>
2025-05-20 19:10:31 +0200

    Files upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//Lista urządzeń
+document.getElementById('devicesList').innerHTML = `
+    <div class="devices">
+<h2>Aktualnie dostępne urządzenia:</h2>
+<ul>
+    <li><a href="rn8pro.html">Redmi Note 8 Pro (begonia)</a></li>
+    <li><a href="klte.html">Samsung S5 (klte)</a></li>
+</ul>
+</div>`
+
+//Navigation bar
+document.getElementById('navbarid').innerHTML = `
+<div class="navbar">
+        <ul>
+            <li><a href="kontakt.html">Kontakt</a></li>
+            <li><a href="informacje.html">Informacje</a></li>
+            <li><a href="changelog.html">Ostatnie zmiany</a></li>
+            <li><a href="projects.html">Projekty</a></li>
+            <li><a href="tutorials.html">Instrukcje</a></li>
+        </ul>
+        <a href="index.html"><img src="CRomsiconorange.png" alt="cus-rom icon"></a>
+        <h2><a href="index.html">cus-rom.com</a></h2>
+        <p>Baza customowych systemów do telefonów</p>
+    </div>
+`
+
+//Stopka
+document.getElementById('footerid').innerHTML = `
+<div class="footer">
+    <p>Copyright: d4gnk</p>
+    <p>Strona powstała w celach edukacyjnych.</p>
+</div>
+`
